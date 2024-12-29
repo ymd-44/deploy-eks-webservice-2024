37cdfe60909e8975e80a2cb3096ffd925a714ed1
ymd <lui199020@gmail.com>
2024-12-29 16:30:18 +0100

    Terraform configuration IAC

---
+++ main.tf
@@ -0,0 +1,159 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.33.0"
+    }
+#Installation de Helm pour la gestion des packages Kubernetes
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
+#Installation de "kubectl"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.26.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Utilisateur IAM : Configurez la première ressource pour le rôle IAM.
+resource "aws_iam_role" "eks-iam-role" {
+    name = "eks-devops24-iam-role"
+
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version" : "2012-10-17",
+ "Statement" : [
+  {
+   "Effect" : "Allow",
+   "Principal" : {
+    "Service" : "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+
+}
+
+# Permissions attachés au role 
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+ role    = aws_iam_role.eks-iam-role.name
+}
+
+# Permissions attachés au nœud de travail EKS.
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-devops24"
+ 
+  assume_role_policy = jsonencode({
+   Statement = [{
+    Action = "sts:AssumeRole"
+    Effect = "Allow"
+    Principal = {
+     Service = "ec2.amazonaws.com"
+    }
+   }]
+   Version = "2012-10-17"
+  })
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role    = aws_iam_role.workernodes.name
+ }
+ 
+ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role    = aws_iam_role.workernodes.name
+ }
+
+
+# Création d’un cluster EKS
+resource "aws_eks_cluster" "eks-devops24" {
+ name = "eks-devops24-cluster"
+ role_arn = aws_iam_role.eks-iam-role.arn
+
+ vpc_config {
+  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+ }
+
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
+}
+
+# Définition des permissions EBS CSI au rôle crée (gestion des volumes EBS)
+module  "ebs_csi_driver_irsa" {
+      source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+      version = "~> 5.20"
+
+      role_name_prefix = "ebs-csi-driver-"
+
+      attach_ebs_csi_policy = true
+
+      oidc_providers = {
+        main = {
+          provider_arn               = aws_eks_cluster.eks-devops24.role_arn
+          namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+        }
+      }
+
+      tags = var.tags
+    }
+# Installation du pilote EBS CSI
+    module "eks_blueprints_addons" {
+      source  = "aws-ia/eks-blueprints-addons/aws"
+      version = "~> 1.1"
+
+      cluster_name      = aws_eks_cluster.eks-devops24.name
+      oidc_provider_arn = aws_eks_cluster.eks-devops24.role_arn
+
+      eks_addons = {
+        aws-ebs-csi-driver = {
+          most_recent              = true
+          service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+        }
+      }
+    }
+
+
+#New : Création des nœuds de travail pour le cluster
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name  = aws_eks_cluster.eks-devops24.name
+  node_group_name = "eks-devops24-workernodes"
+  node_role_arn  = aws_iam_role.workernodes.arn
+  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
+  instance_types = ["t3.xlarge"]
+ 
+  scaling_config {
+   desired_size = 1
+   max_size   = 1
+   min_size   = 1
+  }
+ 
+  depends_on = [
+   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+ }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "subnet_id_1" {
+  type = string
+  default = "first_subnet_id"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "second_subnet_id"
+ }

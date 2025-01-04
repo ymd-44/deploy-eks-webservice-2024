566599e2aacca880853aaff5c2315f62ee3cf6ae
ymd <lui199020@gmail.com>
2025-01-04 22:29:58 +0100

    correction subnets & dependencies jobs

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "first_subnet_id"
+  default = "subnet-040d4247517df5cda"
  }
  
  variable "subnet_id_2" {
   type = string
-  default = "second_subnet_id"
+  default = "subnet-057ac626d3508b6ee"
  }

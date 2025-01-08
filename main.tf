0d2af7bccd2f7a75f2445074f1f4c6d2ff0509a5
ymd <lui199020@gmail.com>
2025-01-08 23:31:53 +0100

    nodes-policies main tf

--- main.tf
+++ main.tf
@@ -129,9 +129,9 @@ resource "aws_eks_node_group" "worker-node-group" {
   }
  
   depends_on = [
-   aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+   aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+   aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+   #aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
  }
 

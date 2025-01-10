97306234f6a24f6912eb76829d5cd2e81225bbcb
ymd <lui199020@gmail.com>
2025-01-10 15:15:47 +0100

    Rollback old version

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "eks-iam-role" {
   {
    "Effect" : "Allow",
    "Principal" : {
-    "Service" : "ec2.amazonaws.com"
+    "Service" : "eks.amazonaws.com"
    },
    "Action": "sts:AssumeRole"
   }

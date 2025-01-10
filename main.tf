4d50a85831dd5296a0d2400967988284cc05dc2a
ymd <lui199020@gmail.com>
2025-01-10 17:14:12 +0100

    delete worker nodes group

--- main.tf
+++ main.tf
@@ -114,27 +114,6 @@ resource "aws_eks_cluster" "eks-devops24" {
  ]
 }
 
-#New : Création des nœuds de travail pour le cluster
-resource "aws_eks_node_group" "worker-node-group" {
-  cluster_name  = aws_eks_cluster.eks-devops24.name
-  node_group_name = "eks-devops24-workernodes"
-  node_role_arn  = aws_iam_role.workernodes.arn
-  subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
-  instance_types = ["t3.xlarge"]
- 
-  scaling_config {
-   desired_size = 1
-   max_size   = 1
-   min_size   = 1
-  }
- 
-  depends_on = [
-   aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-   aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-   #aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
-  ]
- }
-
 #CloudWatch : monitoring aws
 resource "aws_cloudwatch_dashboard" "demo-dashboard" {
   dashboard_name = "demo-dashboard-EKS"

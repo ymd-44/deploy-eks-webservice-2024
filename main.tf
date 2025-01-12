2436f09c8fc4a9bc23896682865d537dfa0b9de6
ymd <10689237+ymd-44@users.noreply.github.com>
2025-01-12 21:12:24 +0100

    lifecycle: external changes without TF plan difference

--- main.tf
+++ main.tf
@@ -132,13 +132,16 @@ resource "aws_eks_node_group" "worker-node-group" {
   node_group_name = "eks-devops24-workernodes"
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids   = [var.subnet_id_1, var.subnet_id_2]
-  capacity_time = "ON_DEMAND"
   instance_types = ["m5.large"]
  
   scaling_config {
    desired_size = 1
-   max_size   = 10
-   min_size   = 0
+   max_size   = 2
+   min_size   = 1
+  }
+
+  update_config {
+    max_unavailable = 1
   }
  
   depends_on = [
@@ -146,6 +149,11 @@ resource "aws_eks_node_group" "worker-node-group" {
    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
+  
+  # Optional: Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
  }
 
 #CloudWatch : monitoring aws
